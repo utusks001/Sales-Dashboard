0a78c19d8ca2f0dd6f9ea0e71c46d7d65be1fa69
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2024-06-13 13:31:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -92,8 +92,8 @@ st.markdown("##")
 # TOP KPI's
 total_sales = int(df_selection["Total"].sum())
 total_sales = total_sales * 16500
-average_sale_by_transaction = round(df_selection["Total"].mean())
-average_sale_by_transaction = int(average_sale_by_transaction) * 16500
+average_sale_by_transaction = int(round(df_selection["Total"].mean()))
+average_sale_by_transaction = average_sale_by_transaction * 16500
 average_rating = round(df_selection["Rating"].mean(), 1)
 star_rating = ":star:" * int(round(average_rating, 0))
 
