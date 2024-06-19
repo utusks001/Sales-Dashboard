bdf74a331ff2a816dd56746c3a5d33c35431f6f3
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2024-06-19 07:27:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,156 +7,120 @@ from matplotlib.gridspec import GridSpec
 import seaborn as sns # pip install seaborn --user
 import streamlit as st  # pip install streamlit --user
 
-st.set_page_config(page_title="Sales Dashboard", layout="wide")
+st.set_page_config(page_title="Financial Dashboard", layout="wide")
 
-# ---- READ EXCEL ----
-@st.cache_data
-def get_data_from_excel():
-    df = pd.read_excel(
-        # io="supermarkt_sales.xlsx",
-        io="financial_sample.xlsx",
-        engine="openpyxl",
-        sheet_name="Sales",
-        skiprows=3,
-        usecols="B:R",
-        nrows=700,
-    )
-    # Add 'hour' column to dataframe
-    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+# Fungsi untuk membaca data dari file Excel
+def read_excel_data():
+    file_path = "financial_sample.xlsx"
+    sheet_name = "Sales"
+    nrows = 700
+    df = pd.read_excel(file_path, sheet_name=sheet_name, nrows=nrows)
     return df
 
-df = get_data_from_excel()
+# Membaca data dari file Excel
+data = read_excel_data()
 
-# # ---- SIDEBAR ----
-# st.sidebar.header("Please Filter Here:")
-# city = st.sidebar.multiselect(
-#     "Select the City:",
-#     options=df["City"].unique(),
-#     default=df["City"].unique()
-# )
+# def get_data_from_csv():
+#     df = pd.read_csv("financials.csv", sep=",")  # Membaca CSV dengan delimiter koma
+#     return df
 
-# customer_type = st.sidebar.multiselect(
-#     "Select the Customer Type:",
-#     options=df["Customer_type"].unique(),
-#     default=df["Customer_type"].unique(),
-# )
-
-# gender = st.sidebar.multiselect(
-#     "Select the Gender:",
-#     options=df["Gender"].unique(),
-#     default=df["Gender"].unique()
-# )
-
-# df_selection = df.query(
-#     "City == @city & Customer_type ==@customer_type & Gender == @gender"
-# )
-
-# # Check if the dataframe is empty:
-# if df_selection.empty:
-#     st.warning("No data available based on the current filter settings!")
-#     st.stop() # This will halt the app from further execution.
+# # Membaca data dari file CSV
+# data = get_data_from_csv()
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
 
-# all_cities = df["City"].unique()
-all_cities = df["Country"].unique()
-city_selection = st.sidebar.checkbox("Select All Country", value=True)
-if city_selection:
-    selected_cities = all_cities
+# Filter untuk negara
+all_countries = data["Country"].unique()
+country_selection = st.sidebar.checkbox("Select All Country", value=True)
+if country_selection:
+    selected_countries = all_countries
 else:
-    selected_cities = st.sidebar.multiselect("Select the Country:", options=all_cities, default=all_cities)
+    selected_countries = st.sidebar.multiselect("Select the Country:", options=all_countries, default=all_countries)
 
-all_customer_types = df["Segment"].unique()
-customer_type_selection = st.sidebar.checkbox("Select All Segment", value=True)
-if customer_type_selection:
-    selected_customer_types = all_customer_types
+# Filter untuk segmen pelanggan
+all_segments = data["Segment"].unique()
+segment_selection = st.sidebar.checkbox("Select All Segment", value=True)
+if segment_selection:
+    selected_segments = all_segments
 else:
-    selected_customer_types = st.sidebar.multiselect("Select the Segment:", options=all_customer_types, default=all_customer_types)
+    selected_segments = st.sidebar.multiselect("Select the Segment:", options=all_segments, default=all_segments)
 
-all_genders = df["Segment"].unique()
-gender_selection = st.sidebar.checkbox("Select All Segment", value=True)
-if gender_selection:
-    selected_genders = all_genders
+# Filter untuk produk 
+all_products = data["Product"].unique() 
+product_selection = st.sidebar.checkbox("Select All Products", value=True)
+if product_selection:
+    selected_products = all_products
 else:
-    selected_genders = st.sidebar.multiselect("Select the Segment:", options=all_genders, default=all_genders)
+    selected_products = st.sidebar.multiselect("Select the Product:", options=all_products, default=all_products)
 
-df_selection = df.query(
-    "City in @selected_cities & Customer_type in @selected_customer_types & Gender in @selected_genders"
+
+# Filter untuk profit
+all_profits = data["Profit"].unique()
+profit_selection = st.sidebar.checkbox("Select All Profits", value=True)
+if profit_selection:
+    selected_profits = all_profits
+else:
+    selected_profits = st.sidebar.multiselect("Select the Profit:", options=all_profits, default=all_profits)
+
+# Pembersihan data untuk nilai non-numerik di kolom Profit
+df_selection = data.copy()  # Buat salinan dataframe untuk diubah
+
+# Ganti nilai non-numerik ("-") dengan NaN
+df_selection["Profit"] = pd.to_numeric(df_selection["Profit"], errors="coerce")
+
+# Hapus baris dengan NaN di kolom Profit
+df_selection = df_selection.dropna(subset=["Profit"])
+
+
+# Add 'Bulan' column to dataframe
+df_selection["Bulan"] = pd.to_datetime(df_selection["Date"], format='%Y-%m')
+
+# Add 'Tahun' column to dataframe
+df_selection["Tahun"] = pd.to_datetime(df_selection["Date"], format='%Y')
+
+# Seleksi dataframe berdasarkan filter yang dipilih
+df_selection = df_selection.query(
+    "Country in @selected_countries & Segment in @selected_segments & Product in @selected_products & Profit in @selected_profits"
 )
 
-# Check if the dataframe is empty:
+# Cek apakah dataframe kosong:
 if df_selection.empty:
     st.warning("No data available based on the current filter settings!")
-    st.stop() # This will halt the app from further execution.
+    st.stop()  # Ini akan menghentikan aplikasi dari eksekusi lebih lanjut.
 
 # ---- MAINPAGE ----
 st.title("Financial Dashboard")
 st.markdown("##")
 
-# # TOP KPI's
-# total_sales = int(df_selection["Total"].sum())
-# average_sale_by_transaction = round(df_selection["Total"].mean())
-# average_rating = round(df_selection["Rating"].mean(), 1)
-# star_rating = ":star:" * int(round(average_rating, 0))
-
-
-# left_column, middle_column, right_column = st.columns(3)
-# with left_column:
-#     st.subheader("Total Sales:")
-#     st.subheader(f"US$ {total_sales:,}")
-# with middle_column:
-#     st.subheader("Average Sales Per Transaction:")
-#     st.subheader(f"US$ {average_sale_by_transaction}")
-# with right_column:
-#     st.subheader("Average Rating:")
-#     st.subheader(f"{int(average_rating)}/10 {star_rating}")
 
 # TOP KPI's
-total_sales = int(df_selection["Sales"].sum())
-total_sales = total_sales * 16500
-average_sale_by_transaction = int(round(df_selection["Sales"].mean()))
-average_sale_by_transaction = average_sale_by_transaction * 16500
-average_rating = round(df_selection["Rating"].mean(), 1)
-star_rating = ":star:" * int(round(average_rating, 0))
-
+total_sales = int(df_selection["Sales"].sum() * 16500)  # Menghitung total sales 
+average_sale_by_transaction = int(round(df_selection["Sales"].mean() * 16500))  # Menghitung rata-rata sales per transaksi
+total_profit = int(df_selection["Profit"].sum() * 16500) # Menghitung total profit
+presentasi = total_profit / total_sales * 100
 
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
-    st.subheader("Total Sales:")
+    st.subheader("Total Sales :")
     st.subheader(f"Rp. {total_sales:,}")
 with middle_column:
-    st.subheader("Average Sales Per Transaction:")
+    st.subheader("Avg Sales/Transaction :")
     st.subheader(f"Rp. {average_sale_by_transaction:,}")
 with right_column:
-    st.subheader("Average Rating:")
-    st.subheader(f"{int(average_rating)}/10 {star_rating}")
+    st.subheader("Total Profit :")
+    st.subheader(f"Rp. {total_profit:,} ({presentasi:.2f} %)")
 
 st.markdown("""---""")
 
-# # SALES BY PRODUCT LINE [BAR CHART]
-# sales_by_product_line = df_selection.groupby(by=["Product line"])[["Total"]].sum().sort_values(by="Total")
-# fig_product_sales = px.bar(
-#     sales_by_product_line,
-#     x="Total",
-#     y=sales_by_product_line.index,
-#     orientation="h",
-#     title="<b>Sales by Product Line</b>",
-#     color_discrete_sequence=["#005F85"] * len(sales_by_product_line),
-#     template="plotly_white",
-# )
-# fig_product_sales.update_layout(
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     xaxis=(dict(showgrid=False))
-# )
 
-# SALES BY PRODUCT LINE [BAR CHART]
+# SALES BY PRODUCT [BAR CHART]
 sales_by_product_city = df_selection.groupby(by=["Product", "Country"])[["Sales"]].sum().reset_index()
 fig_product_sales = px.bar(
     sales_by_product_city,
-    x="Sales",
-    y="Product",
-    orientation="h",
+    y="Sales",
+    x="Product",
+    # orientation="h",
     title="<b>Sales by Product</b>",
     color="Country",
     template="plotly_white",
@@ -178,33 +142,21 @@ fig_product_sales.update_layout(
     xaxis=dict(showgrid=False)
 )
 
-# # SALES BY HOUR [LINE CHART]
-# sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum().reset_index()
-# fig_hourly_sales = px.line(
-#     sales_by_hour,
-#     x=sales_by_hour.index,
-#     y="Total",
-#     title="<b>Sales by Hour</b>",
-#     color_discrete_sequence=["#0027B8"] * len(sales_by_hour),
-#     template="plotly_white",
-# )
-# fig_hourly_sales.update_layout(
-#     xaxis=dict(tickmode="linear"),
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     yaxis=(dict(showgrid=False)),
-# )
+# SALES BY MONTH [LINE CHART]
+# Group by Month and Country, summing Sales
+sales_by_hour_city = df_selection.groupby(by=["Bulan", "Country"])[["Sales"]].sum().reset_index()
 
-# SALES BY HOUR [LINE CHART]
-sales_by_hour_city = df_selection.groupby(by=["Month", "Country"])[["Sales"]].sum().reset_index()
+# Create the line plot
 fig_hourly_sales = px.line(
     sales_by_hour_city,
-    x="Month",
+    x="Bulan",
     y="Sales",
     title="<b>Sales by Month</b>",
     color="Country",
     template="plotly_white",
 )
-# Mengubah font dan ukuran judul
+
+# Customize layout
 fig_hourly_sales.update_layout(
     title={
         'text': "<b>Sales by Month</b>",
@@ -225,7 +177,7 @@ left_column, right_column = st.columns(2)
 left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 right_column.plotly_chart(fig_product_sales, use_container_width=True)
 
-# SALES BY PAYMENT [BAR CHART]
+# SALES BY SEGMENT [BAR CHART]
 sales_by_payment = df_selection.groupby(by=["Segment","Country"])[["Sales"]].sum().reset_index()
 fig_payment_sales = px.bar(
     sales_by_payment,
@@ -236,11 +188,7 @@ fig_payment_sales = px.bar(
     barmode="group",
     template="plotly_white",
 )
-# fig_payment_sales.update_layout(
-#     xaxis=dict(tickmode="linear"),
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     yaxis=dict(showgrid=False),
-# )
+
 # Mengubah font dan ukuran judul
 fig_payment_sales.update_layout(
     title={
@@ -258,7 +206,7 @@ fig_payment_sales.update_layout(
     xaxis=dict(showgrid=False)
 )
 
-# SALES BY CITY [PIE CHART]
+# SALES BY COUNTRY [PIE CHART]
 sales_by_City = df_selection.groupby(by=["Country"])[["Sales"]].sum()
 fig_City_sales = px.pie(
     sales_by_City,
