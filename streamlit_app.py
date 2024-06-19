1c7a7fec4393b685bdc0d29acc4c6fb0c073e0e5
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2024-06-19 11:15:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -223,153 +223,153 @@ left_column, right_column = st.columns(2)
 left_column.plotly_chart(fig_payment_sales, use_container_width=True)
 right_column.plotly_chart(fig_City_sales, use_container_width=True)
 
-st.markdown("""---""")
+# st.markdown("""---""")
 
-# Group data by Discount Band
-grouped_discount = data.groupby('Discount Band').agg({'Sales': 'sum'})
-grouped_discount = grouped_discount.sort_values(by='Sales', ascending=False)
+# # Group data by Discount Band
+# grouped_discount = data.groupby('Discount Band').agg({'Sales': 'sum'})
+# grouped_discount = grouped_discount.sort_values(by='Sales', ascending=False)
 
-# Membersihkan data: menghapus tanda kurung dan mengganti '-' dengan NaN
-data[['Manufacturing Price', 'Sale Price', 'Sales', 'Profit']] = (
-    data[['Manufacturing Price', 'Sale Price', 'Sales', 'Profit']]
-    .replace('-', np.nan)
-    .replace(r'\((.*?)\)', r'-\1', regex=True)
-    .replace(',', '', regex=True)
-    .astype(float)
-)
+# # Membersihkan data: menghapus tanda kurung dan mengganti '-' dengan NaN
+# data[['Manufacturing Price', 'Sale Price', 'Sales', 'Profit']] = (
+#     data[['Manufacturing Price', 'Sale Price', 'Sales', 'Profit']]
+#     .replace('-', np.nan)
+#     .replace(r'\((.*?)\)', r'-\1', regex=True)
+#     .replace(',', '', regex=True)
+#     .astype(float)
+# )
 
-# Menghitung korelasi
-correlation = data[['Manufacturing Price', 'Sale Price', 'Sales', 'Profit']].corr()
+# # Menghitung korelasi
+# correlation = data[['Manufacturing Price', 'Sale Price', 'Sales', 'Profit']].corr()
 
-# Membuat subplots
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+# # Membuat subplots
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
 
-# Mengubah background color pada figure
-fig.patch.set_facecolor('#00172B')  # Ganti dengan warna latar belakang yang diinginkan
+# # Mengubah background color pada figure
+# fig.patch.set_facecolor('#00172B')  # Ganti dengan warna latar belakang yang diinginkan
 
-# Plot Sales by Discount Band
-ax1.bar(grouped_discount.index, grouped_discount['Sales'], color='#E694FF', alpha=0.7)
-ax1.set_title('Sales by Discount Band', fontsize=14, color='white')  # Warna judul
-ax1.set_xlabel('Discount Band', fontsize=12, color='white')  # Warna label x-axis
-ax1.set_ylabel('Sales', fontsize=12, color='white')  # Warna label y-axis
-ax1.grid(axis='y', color='gray', linestyle='--', linewidth=0.5)
-ax1.set_facecolor('#00172B')  # Background color pada ax1
+# # Plot Sales by Discount Band
+# ax1.bar(grouped_discount.index, grouped_discount['Sales'], color='#E694FF', alpha=0.7)
+# ax1.set_title('Sales by Discount Band', fontsize=14, color='white')  # Warna judul
+# ax1.set_xlabel('Discount Band', fontsize=12, color='white')  # Warna label x-axis
+# ax1.set_ylabel('Sales', fontsize=12, color='white')  # Warna label y-axis
+# ax1.grid(axis='y', color='gray', linestyle='--', linewidth=0.5)
+# ax1.set_facecolor('#00172B')  # Background color pada ax1
 
-# Mengubah warna tick labels pada x-axis dan y-axis
-ax1.tick_params(axis='x', colors='white')
-ax1.tick_params(axis='y', colors='white')
+# # Mengubah warna tick labels pada x-axis dan y-axis
+# ax1.tick_params(axis='x', colors='white')
+# ax1.tick_params(axis='y', colors='white')
 
-# Mengubah warna sumbu x dan y
-ax1.spines['bottom'].set_color('white')
-ax1.spines['left'].set_color('white')
-ax1.spines['top'].set_color('white')
-ax1.spines['right'].set_color('white')
+# # Mengubah warna sumbu x dan y
+# ax1.spines['bottom'].set_color('white')
+# ax1.spines['left'].set_color('white')
+# ax1.spines['top'].set_color('white')
+# ax1.spines['right'].set_color('white')
 
-# Plot heatmap of correlation
-sns.heatmap(correlation, annot=True, cmap='coolwarm', center=0, ax=ax2, cbar_kws={"shrink": .8}, annot_kws={"color": "black"})
-ax2.set_title('Correlation Heatmap', fontsize=14, color='white')  # Warna judul
-ax2.set_facecolor('#00172B')  # Background color pada ax2
+# # Plot heatmap of correlation
+# sns.heatmap(correlation, annot=True, cmap='coolwarm', center=0, ax=ax2, cbar_kws={"shrink": .8}, annot_kws={"color": "black"})
+# ax2.set_title('Correlation Heatmap', fontsize=14, color='white')  # Warna judul
+# ax2.set_facecolor('#00172B')  # Background color pada ax2
 
-# Mengubah warna label pada heatmap
-for text in ax2.get_xticklabels():
-    text.set_color('white')
-for text in ax2.get_yticklabels():
-    text.set_color('white')
+# # Mengubah warna label pada heatmap
+# for text in ax2.get_xticklabels():
+#     text.set_color('white')
+# for text in ax2.get_yticklabels():
+#     text.set_color('white')
 
-# Mengubah warna tick labels pada heatmap
-ax2.tick_params(axis='x', colors='white')
-ax2.tick_params(axis='y', colors='white')
+# # Mengubah warna tick labels pada heatmap
+# ax2.tick_params(axis='x', colors='white')
+# ax2.tick_params(axis='y', colors='white')
 
-# Mengubah warna sumbu x dan y pada heatmap
-ax2.spines['bottom'].set_color('white')
-ax2.spines['left'].set_color('white')
-ax2.spines['top'].set_color('white')
-ax2.spines['right'].set_color('white')
+# # Mengubah warna sumbu x dan y pada heatmap
+# ax2.spines['bottom'].set_color('white')
+# ax2.spines['left'].set_color('white')
+# ax2.spines['top'].set_color('white')
+# ax2.spines['right'].set_color('white')
 
-# Menambahkan grid pada heatmap
-ax2.grid(linewidth=0.5, linestyle='--', color='gray')
+# # Menambahkan grid pada heatmap
+# ax2.grid(linewidth=0.5, linestyle='--', color='gray')
 
-# Menambahkan gradient color bar
-cbar = ax2.collections[0].colorbar
-cbar.ax.tick_params(labelcolor='white')
-cbar.set_label(label='Profit', color='white')  # Mengubah warna label degradasi warna
+# # Menambahkan gradient color bar
+# cbar = ax2.collections[0].colorbar
+# cbar.ax.tick_params(labelcolor='white')
+# cbar.set_label(label='Profit', color='white')  # Mengubah warna label degradasi warna
 
-# Menambahkan warna latar belakang pada figure
-plt.subplots_adjust(wspace=0.4)
-st.pyplot(fig)
+# # Menambahkan warna latar belakang pada figure
+# plt.subplots_adjust(wspace=0.4)
+# st.pyplot(fig)
 
-# Create a new dataframe with total monthly profit for each product
-data['Date'] = pd.to_datetime(data['Date'])
-monthly_product_profit = data.groupby([data['Date'].dt.year, data['Date'].dt.month, 'Product']).agg({'Profit': 'sum'})
-monthly_product_profit.index.names = ['Year', 'Month', 'Product']
-monthly_product_profit.reset_index(inplace=True)
+# # Create a new dataframe with total monthly profit for each product
+# data['Date'] = pd.to_datetime(data['Date'])
+# monthly_product_profit = data.groupby([data['Date'].dt.year, data['Date'].dt.month, 'Product']).agg({'Profit': 'sum'})
+# monthly_product_profit.index.names = ['Year', 'Month', 'Product']
+# monthly_product_profit.reset_index(inplace=True)
 
-# Create a 'Year-Month' column for easier plotting
-monthly_product_profit['Year-Month'] = pd.to_datetime(monthly_product_profit[['Year', 'Month']].assign(day=1))
+# # Create a 'Year-Month' column for easier plotting
+# monthly_product_profit['Year-Month'] = pd.to_datetime(monthly_product_profit[['Year', 'Month']].assign(day=1))
 
-# Create a pivot table of 'Profit' with 'Product' and 'Discount Band' as dimensions
-product_discount_profit = data.pivot_table(values='Profit', index='Product', columns='Discount Band', aggfunc='sum')
+# # Create a pivot table of 'Profit' with 'Product' and 'Discount Band' as dimensions
+# product_discount_profit = data.pivot_table(values='Profit', index='Product', columns='Discount Band', aggfunc='sum')
 
-# Membuat subplots
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+# # Membuat subplots
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
 
-# Plot monthly profit for each product
-for product in monthly_product_profit['Product'].unique():
-    product_data = monthly_product_profit[monthly_product_profit['Product'] == product]
-    ax1.plot(product_data['Year-Month'], product_data['Profit'], label=product)
-ax1.set_title('Monthly Profit by Product', fontsize=14, color='white')
-ax1.set_xlabel('Date', fontsize=12, color='white')
-ax1.set_ylabel('Profit', fontsize=12, color='white')
-ax1.legend()
-ax1.grid()
-ax1.set_facecolor('#00172B')
+# # Plot monthly profit for each product
+# for product in monthly_product_profit['Product'].unique():
+#     product_data = monthly_product_profit[monthly_product_profit['Product'] == product]
+#     ax1.plot(product_data['Year-Month'], product_data['Profit'], label=product)
+# ax1.set_title('Monthly Profit by Product', fontsize=14, color='white')
+# ax1.set_xlabel('Date', fontsize=12, color='white')
+# ax1.set_ylabel('Profit', fontsize=12, color='white')
+# ax1.legend()
+# ax1.grid()
+# ax1.set_facecolor('#00172B')
 
-# Mengubah warna tick labels pada x-axis dan y-axis
-ax1.tick_params(axis='x', colors='white')
-ax1.tick_params(axis='y', colors='white')
+# # Mengubah warna tick labels pada x-axis dan y-axis
+# ax1.tick_params(axis='x', colors='white')
+# ax1.tick_params(axis='y', colors='white')
 
-# Mengubah warna sumbu x dan y
-ax1.spines['bottom'].set_color('white')
-ax1.spines['left'].set_color('white')
-ax1.spines['top'].set_color('white')
-ax1.spines['right'].set_color('white')
+# # Mengubah warna sumbu x dan y
+# ax1.spines['bottom'].set_color('white')
+# ax1.spines['left'].set_color('white')
+# ax1.spines['top'].set_color('white')
+# ax1.spines['right'].set_color('white')
 
-# Plot a heatmap of the pivot table (contoh plot kedua)
-sns.heatmap(product_discount_profit, annot=True, fmt='.0f', cmap='viridis', ax=ax2, cbar_kws={'label': 'Profit'},
-            annot_kws={'color': 'black', 'fontsize': 10})  # Menyesuaikan warna teks dan ukuran font
-ax2.set_title('Profit across Products and Discount Bands', fontsize=14, color='white')
-ax2.set_xlabel('Discount Band', fontsize=12, color='white')
-ax2.set_ylabel('Product', fontsize=12, color='white')
+# # Plot a heatmap of the pivot table (contoh plot kedua)
+# sns.heatmap(product_discount_profit, annot=True, fmt='.0f', cmap='viridis', ax=ax2, cbar_kws={'label': 'Profit'},
+#             annot_kws={'color': 'black', 'fontsize': 10})  # Menyesuaikan warna teks dan ukuran font
+# ax2.set_title('Profit across Products and Discount Bands', fontsize=14, color='white')
+# ax2.set_xlabel('Discount Band', fontsize=12, color='white')
+# ax2.set_ylabel('Product', fontsize=12, color='white')
 
-# Mengubah warna label pada heatmap
-for text in ax2.get_xticklabels():
-    text.set_color('white')
-for text in ax2.get_yticklabels():
-    text.set_color('white')
+# # Mengubah warna label pada heatmap
+# for text in ax2.get_xticklabels():
+#     text.set_color('white')
+# for text in ax2.get_yticklabels():
+#     text.set_color('white')
 
-# Mengubah warna tick labels pada heatmap
-ax2.tick_params(axis='x', colors='white')
-ax2.tick_params(axis='y', colors='white')
+# # Mengubah warna tick labels pada heatmap
+# ax2.tick_params(axis='x', colors='white')
+# ax2.tick_params(axis='y', colors='white')
 
-# Mengubah warna sumbu x dan y pada heatmap
-ax2.spines['bottom'].set_color('white')
-ax2.spines['left'].set_color('white')
-ax2.spines['top'].set_color('white')
-ax2.spines['right'].set_color('white')
+# # Mengubah warna sumbu x dan y pada heatmap
+# ax2.spines['bottom'].set_color('white')
+# ax2.spines['left'].set_color('white')
+# ax2.spines['top'].set_color('white')
+# ax2.spines['right'].set_color('white')
 
-# Menambahkan grid pada heatmap
-ax2.grid(linewidth=0.5, linestyle='--', color='gray')
+# # Menambahkan grid pada heatmap
+# ax2.grid(linewidth=0.5, linestyle='--', color='gray')
 
-# Menambahkan gradient color bar
-cbar = ax2.collections[0].colorbar
-cbar.ax.tick_params(labelcolor='white')
-cbar.set_label(label='Profit', color='white')  # Mengubah warna label degradasi warna
+# # Menambahkan gradient color bar
+# cbar = ax2.collections[0].colorbar
+# cbar.ax.tick_params(labelcolor='white')
+# cbar.set_label(label='Profit', color='white')  # Mengubah warna label degradasi warna
 
-# Mengubah background color pada figure
-fig.patch.set_facecolor('#00172B')
+# # Mengubah background color pada figure
+# fig.patch.set_facecolor('#00172B')
 
-# Menampilkan plot di Streamlit
-st.pyplot(fig)
+# # Menampilkan plot di Streamlit
+# st.pyplot(fig)
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
